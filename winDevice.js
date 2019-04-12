c035224c7f2d950decc57ac7d2b47b0aceaa633c
Siddharth <siddmegadeth@gmail.com>
2019-04-12 17:35:17 +0530

    updated WinDevice
    updated WinDevice

--- winDevice.js
+++ winDevice.js
@@ -1,136 +1,80 @@
-(function(window,undefined)
-{
-  function generateContact()
-  {
-    log("Genrating Contact");
-  }
-  var isAuto = false;
-  var devType=false;
-  function winDevice(appName,inject,force)
-  {
-     try{
-      if(window.angular)
-      {
-      isAuto=true;
-      if(!force)
-      {
-      if(document.URL.indexOf('http://') === -1&& document.URL.indexOf('https://') === -1)
-      {
-        console.log("Cordova App");
-        var fileref=document.createElement('script')
-        fileref.setAttribute("type","text/javascript")
-        fileref.setAttribute("src","cordova.js");
-        document.getElementsByTagName("head")[0].appendChild(fileref);
-        devType=true;
-        document.addEventListener("deviceready",function()
-        {
-          console.log("Bootstrapping APP On Cordova"+appName);
-          angular.bootstrap(document,[appName]);
-          log("Device Ready");
-        },false);
-      }
-      else
-      {
-      //  console.log("Running In A Browser");
-        devType=false;
-        window.onload = function()
-        {
-          console.log("Bootstrapping APP On Browser"+appName);
-          angular.bootstrap(document,[appName]);
-          log("Device Ready");
+(function(window, undefined) {
+
+    var isAuto = false;
+    var devType = undefined;
+
+    function winDevice(appName, inject) {
+        try {
+            if (window.angular) {
+                isAuto = true;
+                if (document.URL.indexOf('http://') === -1 && document.URL.indexOf('https://') === -1 || window.cordova) {
+                    console.log("Cordova App");
+                    devType = true;
+                    document.addEventListener("deviceready", function() {
+                        console.log("Bootstrapping APP On Cordova : " + appName);
+                        angular.bootstrap(document, [appName]);
+                        log("Device Ready");
+                        //  $.getScript('https://maps.googleapis.com/maps/api/js?libraries=places&key=AIzaSyAiC98SnMtJTpFFTj5MrkTGmbfIE1n2neY');
+
+                    }, false);
+                } else {
+                    console.log("Running In A Browser");
+                    devType = false;
+                    document.addEventListener("DOMContentLoaded", function() {
+                        console.log("Bootstrapping APP On Browser : " + appName);
+                        angular.bootstrap(document, [appName]);
+
+                        // $.getScript('https://maps.googleapis.com/maps/api/js?libraries=places&key=AIzaSyAiC98SnMtJTpFFTj5MrkTGmbfIE1n2neY');
+
+                        log("Device Ready");
+                    });
+                }
+
+
+                var infoType = {};
+                this.enable = function(flag) {
+                    infoType.log = flag;
+                    if (flag) {
+                        log = console.log.bind(console);
+                        error = console.error.bind(console);
+                        warn = console.warn.bind(console);
+                    }
+                    if (!flag) {
+                        log = function() {};
+                        error = function() {};
+                        warn = function() {};
+                        console.log = function() {};
+                        console.error = function() {};
+                        console.warn = function() {};
+                    }
+                    return this;
+                }
+                this.device = function() {
+                    if (window.angular) {
+                        console.log("App Init with Dependency " + appName);
+                        return angular.module(appName, inject);
+                        return this;
+                    } else {
+                        console.error("Angularjs Not Found");
+                    }
+                };
+                this.info = function() {
+                    if (devType) {
+                        log("Emulation For Cordova : true");
+                    } else {
+                        log("Emulation For Browser : true");
+                    }
+                    log("Logging : " + infoType.log);
+                    return this;
+                }
+
+            } else {
+                console.error("Angular Not Found");
+            }
+        } catch (e) {
+            //Error
+            return e;
         }
-      }
     }
-    else
-    {
-      console.log("Forced");
-      console.log("Cordova App");
-      var fileref=document.createElement('script')
-      fileref.setAttribute("type","text/javascript")
-      fileref.setAttribute("src","cordova.js");
-      document.getElementsByTagName("head")[0].appendChild(fileref);
-      devType=true;
-      document.addEventListener("deviceready",function()
-      {
-        console.log("Bootstrapping APP On Cordova"+appName);
-        angular.bootstrap(document,[appName]);
-        log("Device Ready");
-      },false);
-    }
-    var infoType  = {};
-    this.log = function(flag)
-    {
-      infoType.log = flag;
-      if(flag)
-      {
-        log = console.log.bind(console);
-        error = console.error.bind(console);
-        warn = console.warn.bind(console);
-      }
-      if(!flag)
-      {
-        log = function(){};
-        error = function(){};
-        warn = function(){};
-        console.log = function(){};
-        console.error = function(){};
-        console.warn = function(){};
-      }
-      return this;
-    }
-    this.device = function()
-    {
-      if(window.angular)
-      {
-       console.log("App Init with Dependency "+appName);
-       return angular.module(appName,inject);
-       return this;
-      }
-      else
-      {
-        console.error("Angularjs Not Found");
-      }
-    };
-    this.info = function()
-    {
-      if(infoType.device)
-      {
-        log("Emulation For Cordova : true");
-      }
-      else
-      {
-        log("Emulation For Browser : true");
-      }
-      log("Logging : "+infoType.log);
-      log("Is Auto : ")
-      return this;
-    }
-    this.cordova = {};
-    this.cordova.contacts = function(success,failure)
-    {
-        log("Cordova Contact API");
-        if(navigator.contacts)
-        {
-          success();
-        }
-        else
-        {
-          generateContact();
-          failure();
-        }
-        return this;
-    }
-  }
-  else
-  {
-    console.error("Angularjs Not Found");
-  }
-  }
-  catch(e)
-  {
-    //Error
-    return e;
-  }
- }
-  window.winDevice = winDevice;
+    window.winDevice = winDevice;
 })(window)
